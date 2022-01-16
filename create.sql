914e95911d35a1583fc340a94beb161429c8dc08
lichen <lichen0426@stu.xidian.edu.cn>
2022-01-16 13:24:56 +0800

    🥲 Add sql to further learning

--- create.sql
+++ create.sql
@@ -3,7 +3,7 @@
 # @describe This is the SQL script to create table you need and
 #           insert some test records.
 
-create database MyBatis if not exists;
+create database if not exists MyBatis;
 
 use MyBatis;
 
@@ -16,3 +16,17 @@ create table user (
 insert into user values
 (1, 'Tommy', 15000),
 (2, 'Jack', 21000);
+
+# if you want to learn how to join table, you must excute the following sql
+
+alter table user add dep_id int not null;
+
+create table department (
+    id int primary key,
+    name varchar(255) not null
+);
+
+insert into department
+values
+    (1, 'Developer Dep.'),
+    (2, 'Finance Dep.');
